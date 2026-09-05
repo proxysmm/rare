8378e3e6fce7c385cfeff9356751136695aceddb
proxysmm <lsssmar17@gmail.com>
2026-09-05 05:56:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -988,4 +988,5 @@ def smm_api():
         return jsonify({"error": "Incorrect action"}), 400
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000) 0
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
